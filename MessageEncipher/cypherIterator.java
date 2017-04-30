860495f7688dddc8fd0aa66c5c5a18774fc3d2c4
DESKTOP-8OLJ4T9\ujust <berniem@bu.edu>
2017-04-30 09:05:14 -0400

    Working Code

--- MessageEncipher/cypherIterator.java
+++ MessageEncipher/cypherIterator.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import java.util.*;
 public class cypherIterator {
     
@@ -29,75 +29,5 @@ public class cypherIterator {
     public String completeRand(){
         
         return Cypher.completeRand(input);
-    }
-    
-    public static void main(String[] args){
-        
-        cypherIterator n = new cypherIterator("Ab!C");
-        
-        //System.out.println(n.setIncrease());
-        
-        //System.out.println(n.setDecrease());
-        
-        //System.out.println(n.completeRand());
-        
-        System.out.println(n.convertWithRandomTable());
-        
-    }
-    
-    
-    
-    
+    }    
 }
-=======
-import java.util.*;
-public class cypherIterator {
-    
-    Random rand = new Random();
-    int num = rand.nextInt(25) + 1;
-    Cypher n = new Cypher();
-    String input;
-    
-    public cypherIterator(String in){
-        input = in;
-    }
-    
-    public String convertWithRandomTable(){
-        
-        return Cypher.convertWithRandomTable(input);
-    }
-    
-    public String setIncrease(){
-        
-        return Cypher.setIncrease(num, input);
-    }
-    
-    public String setDecrease(){
-        
-        return Cypher.setDecrease(num, input);
-    }
-    
-    public String completeRand(){
-        
-        return Cypher.completeRand(input);
-    }
-    
-    public static void main(String[] args){
-        
-        cypherIterator n = new cypherIterator("Ab!C");
-        
-        //System.out.println(n.setIncrease());
-        
-        //System.out.println(n.setDecrease());
-        
-        //System.out.println(n.completeRand());
-        
-        System.out.println(n.convertWithRandomTable());
-        
-    }
-    
-    
-    
-    
-}
->>>>>>> 10e7f7ffab1ef5dc876a9d71b0d6b20d7b51f224
